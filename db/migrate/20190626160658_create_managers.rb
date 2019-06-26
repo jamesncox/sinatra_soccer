d39c057e3726d146e21a5f56e4908a27f4d75976
James <jamesncox@yahoo.com>
2019-06-26 11:17:56 -0500

    Fixed file structure.

--- db/migrate/20190626160658_create_managers.rb
+++ db/migrate/20190626160658_create_managers.rb
@@ -3,7 +3,7 @@ class CreateManagers < ActiveRecord::Migration
     create_table :managers do |t|
       t.string :name
       t.string :email 
-      t.string :password_digest.
-    end 
+      t.string :password_digest
+    end  
   end
 end
