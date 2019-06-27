636a3c93ca867c554744760da8dd98a6860f4aa0
James <jamesncox@yahoo.com>
2019-06-27 13:35:11 -0500

    Made edit route team stats controller

--- app/controllers/team_stats_controller.rb
+++ app/controllers/team_stats_controller.rb
@@ -40,6 +40,19 @@ class TeamStatsController < ApplicationController
           redirect to '/login'
         end
       end 
+
+      get '/team_stats/:id/edit' do
+        if logged_in?
+          @teamstat = TeamStat.find_by_id(params[:id])
+          if @teamstat && @teamstat.manager == current_manager 
+            erb :"/team_stats/edit_team_stats"
+          else 
+            redirect to "/team_stats"
+          end
+        else 
+          redirect to "/login" 
+        end 
+      end 
       
 
 end 
