e5b1b61221e416a7354ae6d2018e8042751f1bb9
James <jamesncox@yahoo.com>
2019-06-27 18:26:15 -0500

    fixed new team stats controller and view

--- app/controllers/team_stats_controller.rb
+++ app/controllers/team_stats_controller.rb
@@ -18,11 +18,10 @@ class TeamStatsController < ApplicationController
       
       post '/team_stats' do
         if logged_in?
-          if params[:team_name] != ""  
-            @team_stat = TeamStat.new(params)
+          if params[:team_name] != "" 
+            @team_stat = TeamStat.new(team_name: params[:team_name], points: params[:points], goals_scored: params[:goals_scored])
             @team_stat.manager = current_manager
             @team_stat.save
-            # binding.pry
             redirect to "/team_stats/#{@team_stat.id}"
           else
             redirect to '/team_stats/new'
@@ -43,8 +42,8 @@ class TeamStatsController < ApplicationController
 
       get '/team_stats/:id/edit' do
         if logged_in?
-          @teamstat = TeamStat.find_by_id(params[:id])
-          if @teamstat && @teamstat.manager == current_manager 
+          @team_stat = TeamStat.find_by_id(params[:id])
+          if @team_stat && @teamstat.manager == current_manager 
             erb :"/team_stats/edit_team_stats"
           else 
             redirect to "/team_stats"
