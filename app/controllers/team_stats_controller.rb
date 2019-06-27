d36df7c7cffc03833e6d925edd9a6b43af41a23b
James <jamesncox@yahoo.com>
2019-06-27 10:30:05 -0500

    team stats controller get route

--- app/controllers/team_stats_controller.rb
+++ app/controllers/team_stats_controller.rb
@@ -1,4 +1,11 @@
 class TeamStatsController < ApplicationController
-    
+    get '/team_stats' do 
+        if logged_in?
+         @team_stats = TeamStats.all
+         erb :'team_stats/team_stats'
+       else
+         redirect to '/login'
+       end
+     end 
 
 end 
