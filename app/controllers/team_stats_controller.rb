888a4add2086e2b2641b0ce0fae716541d675ebe
James <jamesncox@yahoo.com>
2019-06-27 10:39:52 -0500

    working on new team stats controller/view

--- app/controllers/team_stats_controller.rb
+++ app/controllers/team_stats_controller.rb
@@ -8,4 +8,29 @@ class TeamStatsController < ApplicationController
        end
      end 
 
+     get '/team_stats/new' do 
+        if logged_in?
+          erb :"team_stats/new"
+        else 
+          redirect to "/login" 
+        end 
+      end 
+      
+      post '/team_stats' do
+        if logged_in?
+          if params[:team_name] != "" && 
+            @team_stat = TeamStats.new(content: params[:content])
+            @team_stat.manager = current_user
+            @team_stat.save
+            # binding.pry
+            redirect to "/team_stats/#{@team_stat.id}"
+          else
+            redirect to '/tweets/new'
+          end 
+        else 
+          redirect to '/login' 
+        end
+      end 
+      
+
 end 
